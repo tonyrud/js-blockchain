7050c0ab7aa776b5a59244c0ae620f38234405e3
tonyrud <tonrudny@gmail.com>
2018-09-08 08:46:29 -0400

    added express routes

--- app.js
+++ app.js
@@ -1,16 +1,42 @@
+const express = require('express')
+const app = express()
+const bodyparser = require('body-parser')
 const Block = require('./block')
 const BlockChain = require('./blockchain')
 const Transaction = require('./transaction')
 
+let transactions = []
 const genesisBlock = new Block()
 const blockchain = new BlockChain(genesisBlock)
 
-const transaction = new Transaction('Mary', 'John', 100)
-const block = blockchain.getNextBlock([transaction])
-blockchain.addBlock(block)
+app.use(bodyparser.json)
 
-const transaction2 = new Transaction('Tony', 'Beth', 1000)
-const block2 = blockchain.getNextBlock([transaction2, transaction])
-blockchain.addBlock(block2)
+app.get('/', (req, res) => {
+  res.send('server running!')
+})
 
-console.log(blockchain)
+app.get('/mine', (req, res) => {
+  const block = blockchain.getNextBlock(transactions)
+  blockchain.addBlock(block)
+  res.json(block)
+})
+
+app.post('/transactions', (req, res) => {
+  console.log('creating transaction')
+
+  const { to, from, amount } = req.body
+
+  const transaction = new Transaction(from, to, amount)
+
+  transactions.push(transaction)
+
+  res.json(transactions)
+})
+
+app.get('/blockchain', (req, res) => {
+  res.json(blockchain)
+})
+
+app.listen(3000, () => {
+  console.log('server started on port 3000')
+})
